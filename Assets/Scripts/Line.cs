1180a72b1b616af6eeed7ad90108ce7f18be8748
Leonid <leonidb93@gmail.com>
2018-06-26 15:43:32 +0200

    start implement data structure for drawings

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+    private Color colour;
+    private Vector3[] positions;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
