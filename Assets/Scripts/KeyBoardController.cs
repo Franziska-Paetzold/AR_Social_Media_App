c8d8914bc2b3001a966d513afe48bfa2709fde35
Franziska Paetzold <s0554620@htw-berlin.de>
2018-07-07 09:39:26 +0200

    Update TextPost button function - missing the emersion of the textview

--- Assets/Scripts/KeyBoardController.cs
+++ Assets/Scripts/KeyBoardController.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class KeyBoardController : MonoBehaviour
 {
 
-    public string textPostContent = "Hello World";
+    private string textPostContent = "";
     private TouchScreenKeyboard keyboard;
-
+  
 
     // Opens native keyboard
     void OnGUI()
     {
-        textPostContent = GUI.TextField(new Rect(10, 10, 200, 30), textPostContent);
+        //GUIStyle kindOfPostButtonStyle = new GUIStyle("kindOfPostButtonStyle");
+        //kindOfPostButtonStyle.fontSize = 20;
+        //bool textPostButton = GUI.Button(new Rect(10, Screen.height - 200, 200, 200), "A", kindOfPostButtonStyle);
+        bool textPostButton = GUI.Button(new Rect(10, Screen.height - 500, 200, 200), "A");
 
-        if (GUI.Button(new Rect(10, 50, 200, 100), "Text post"))
+                                 
+        if (textPostButton)
         {
             keyboard = TouchScreenKeyboard.Open(textPostContent);
+            TouchScreenKeyboard.hideInput = true;
+            textPostContent = GUI.TextField(new Rect(Screen.width/2, Screen.height/2, 200, 200), textPostContent);
         }
 
         if (keyboard != null)
