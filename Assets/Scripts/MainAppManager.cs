90246114ddb0c5f2a8cdab4dc9fa0f11c2089577
Leonid <leonidb93@gmail.com>
2018-07-11 02:09:09 +0200

    Add manager

---
+++ Assets/Scripts/MainAppManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainAppManager : MonoBehaviour {
+
+    public static MainAppManager mainAppManager;
+
+
+	// Use this for initialization
+	void Awake () {
+		if(mainAppManager == null)
+        {
+            mainAppManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
